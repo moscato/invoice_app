b5731db9014df779ecc949fc711a3a62809bec5b
ramosv <56368263+ramosv@users.noreply.github.com>
2023-04-06 18:25:09 -0600

    Changes

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ console.log('app.js is working');
 
 let vic = document.querySelector('.vic');
 let sendBtn = document.querySelector('.sendBtn');
+let number4 = document.querySelector('.number4');
+
+number4.textContent = 'New number 4'
 
 
 sendBtn.addEventListener('click', () => {
