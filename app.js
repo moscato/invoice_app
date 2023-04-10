bb982669cec21a23c1b40c8574513dc658ca78fa
jMoscato <jmoscato14@gmail.com>
2023-04-09 21:11:00 -0600

    first_draft

--- app.js
+++ app.js
@@ -1,14 +1,71 @@
+
+
+let jobIn = document.querySelector('.jobIn');
+let costs = document.querySelector('.costs');
+let inCost = document.querySelector('.inCost');
+let itemIn = document.querySelector('.itemIn');
+let costIn = document.querySelector('.costIn');
+let valIn = document.querySelector('.valIn');
+let jobHere = document.querySelector('.jobHere');
+let costValues = document.querySelector('.costValues');
+let totalCost = document.querySelector('.totalCost');
+
+let noCost = 0;
+
+function sendItems(element, message) {
+    let itemPost = document.createElement("li");
+    itemPost.textContent = message;
+    element.appendChild(itemPost);
+}
+
+
+valIn.addEventListener('click', () => {
+    jobHere.textContent = jobIn.value
+    
+    costVal = itemIn.value + " - $" + costIn.value
+    sendItems(costValues, costVal);
+
+    let allCostVal = noCost += parseInt(costIn.value);
+    totalCost.textContent = '$ ' + allCostVal;
+});
+
+
+
+
+/*
+here
+jobHere
+valIn
+itemCost
+itemName
+incost
+costs
+joibIn
+
+
+
+*/
 console.log('app.js is working');
 
 
 
-let vic = document.querySelector('.vic');
-let sendBtn = document.querySelector('.sendBtn');
-let number4 = document.querySelector('.number4');
-
-number4.textContent = 'New number 4'
 
 
-sendBtn.addEventListener('click', () => {
-    vic.style.color = 'red';
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
